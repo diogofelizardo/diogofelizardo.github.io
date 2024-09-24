c49817e1a99f5d5ebd04967e2fe098ab1e478a89
diogofelizardo <felizardo.diogo@gmail.com>
2024-09-24 20:41:02 -0300

    update package name and add frame slot to Content component

--- src/types.d.ts
+++ src/types.d.ts
@@ -277,10 +277,11 @@ export interface Content extends Omit<Headline, 'classes'>, Widget {
   content?: string;
   image?: string | unknown;
   items?: Array<Item>;
+  frame?: string | unknown;
   columns?: number;
   isReversed?: boolean;
   isAfterContent?: boolean;
   callToAction?: CallToAction;
 }
 
-export interface Contact extends Omit<Headline, 'classes'>, Form, Widget {}
+export interface Contact extends Omit<Headline, 'classes'>, Form, Widget { }
